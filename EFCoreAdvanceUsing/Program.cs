c1b3ec3c2bddb63fe04c961569f55cf54d6aef14
albertzhaoz <szdxzhy@outlook.com>
2022-07-06 22:28:24 +0800

    IQueryable延迟执行，服务器端评估

---
+++ EFCoreAdvanceUsing/Program.cs
@@ -0,0 +1,8 @@
+﻿using EFCoreRelation.DbContexts;
+
+AlbertDbContext albertDbContext = new AlbertDbContext();
+var test = albertDbContext.Articles;
+foreach (var item in test.Where(a => a.ID > 1))
+{
+    Console.WriteLine(item.Content);
+}
